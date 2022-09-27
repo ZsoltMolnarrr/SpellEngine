d5dd94e5d0681a96ab8a61a9af1eaee3740a2792
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-09-27 19:21:18 +0200

    Add ItemStack attribute modifier caching

--- common/src/main/java/net/combatspells/mixin/AttributeAssigner_ItemStackMixin.java
+++ common/src/main/java/net/combatspells/mixin/AttributeAssigner_ItemStackMixin.java
@@ -1,8 +1,11 @@
 package net.combatspells.mixin;
 
+import com.google.common.collect.Multimap;
 import net.combatspells.attribute_assigner.AttributeAssigner;
 import net.combatspells.attribute_assigner.ItemAttributeApplier;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,8 +15,11 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Mixin(ItemStack.class)
 public abstract class AttributeAssigner_ItemStackMixin {
@@ -55,5 +61,33 @@ public abstract class AttributeAssigner_ItemStackMixin {
         } else {
             return new EquipmentSlot[]{ EquipmentSlot.MAINHAND };
         }
+    }
+
+    // Attribute modifier caching
+
+    private final Map<EquipmentSlot, Multimap<EntityAttribute, EntityAttributeModifier>> cachedAttributes = new HashMap<>();
+
+    @Inject(method = "getAttributeModifiers", at = @At("RETURN"))
+    private void getAttributeModifiers_RETURN(EquipmentSlot slot, CallbackInfoReturnable<Multimap<EntityAttribute, EntityAttributeModifier>> cir) {
+        var parsedAttributes = cir.getReturnValue();
+        // System.out.println("Attributes parsed");
+        if (parsedAttributes != null && !parsedAttributes.isEmpty()) {
+            cachedAttributes.put(slot, parsedAttributes);
+        }
+    }
+
+    @Inject(method = "getAttributeModifiers", at = @At("HEAD"), cancellable = true)
+    private void getAttributeModifiers_fromCache(EquipmentSlot slot, CallbackInfoReturnable<Multimap<EntityAttribute, EntityAttributeModifier>> cir) {
+        var cached = cachedAttributes.get(slot);
+        if (cached != null) {
+            // System.out.println("Attributes from cache");
+            cir.setReturnValue(cached);
+            cir.cancel();
+        }
+    }
+
+    @Inject(method = "addAttributeModifier", at = @At("HEAD"))
+    private void addAttributeModifier_HEAD(EntityAttribute attribute, EntityAttributeModifier modifier, EquipmentSlot slot, CallbackInfo ci) {
+        cachedAttributes.clear();
     }
 }
