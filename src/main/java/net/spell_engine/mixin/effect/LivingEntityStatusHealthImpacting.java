938310267441a28e7f86ad3f1bb64c895d1b692d
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-10-19 22:20:27 +0200

    Add `HealthImpacting` interface for status effects, to modify damage and healing taken

--- src/main/java/net/spell_engine/mixin/effect/LivingEntityStatusHealthImpacting.java
+++ src/main/java/net/spell_engine/mixin/effect/LivingEntityStatusHealthImpacting.java
@@ -3,7 +3,7 @@ package net.spell_engine.mixin.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.spell_engine.api.effect.HealingTakenModifier;
+import net.spell_engine.api.effect.HealthImpacting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,25 +15,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Map;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityStatusEffectHealingTaken {
+public abstract class LivingEntityStatusHealthImpacting {
     @Shadow @Final private Map<StatusEffect, StatusEffectInstance> activeStatusEffects;
     private float healingTakenModifier_SpellEngine = 1F;
+    private float damageTakenModifier_SpellEngine = 1F;
     /**
      * `updatePotionVisibility` is called upon effects of the entity are changed.
      */
     @Inject(method = "updatePotionVisibility", at = @At("HEAD"))
     private void updatePotionVisibility_HEAD_SpellEngine_UpdateHealingTaken(CallbackInfo ci) {
-        var newValue = 1F;
+        var healingNewValue = 1F;
+        var damageNewValue = 1F;
         for (var entry : activeStatusEffects.entrySet()) {
             var effect = entry.getKey();
             var instance = entry.getValue();
-            newValue += ((HealingTakenModifier)effect).getHealingTakenModifierPerStack() * (instance.getAmplifier() + 1);
+            var stacks = instance.getAmplifier() + 1;
+            healingNewValue += ((HealthImpacting)effect).getHealingTakenModifierPerStack() * stacks;
+            damageNewValue += ((HealthImpacting)effect).getDamageTakenModifierPerStack() * stacks;
         }
-        healingTakenModifier_SpellEngine = newValue;
+        healingTakenModifier_SpellEngine = healingNewValue;
+        damageTakenModifier_SpellEngine = damageNewValue;
     }
 
-    @ModifyVariable(method = "heal(F)V", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    private float injected(float amount) {
+    @ModifyVariable(method = "heal", at = @At("HEAD"), argsOnly = true)
+    private float modifyHealingTaken_SpellEngine(float amount) {
         return amount * healingTakenModifier_SpellEngine;
+    }
+
+    @ModifyVariable(method = "damage", at = @At("HEAD"), argsOnly = true)
+    public float modifyDamageTaken_SpellEngine(float amount) {
+        return amount * damageTakenModifier_SpellEngine;
     }
 }
--- src/main/java/net/spell_engine/mixin/effect/StatusEffectHealthImpacting.java
+++ src/main/java/net/spell_engine/mixin/effect/StatusEffectHealthImpacting.java
@@ -1,11 +1,21 @@
 package net.spell_engine.mixin.effect;
 
 import net.minecraft.entity.effect.StatusEffect;
-import net.spell_engine.api.effect.HealingTakenModifier;
+import net.spell_engine.api.effect.HealthImpacting;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(StatusEffect.class)
-public class StatusEffectHealingTakenModifier implements HealingTakenModifier {
+public class StatusEffectHealthImpacting implements HealthImpacting {
+    private float damageTakenModifierPerStack_SpellEngine = 0;
+    @Override
+    public float getDamageTakenModifierPerStack() {
+        return damageTakenModifierPerStack_SpellEngine;
+    }
+    @Override
+    public void setDamageTakenModifierPerStack(float modifierPerStack) {
+        damageTakenModifierPerStack_SpellEngine = modifierPerStack;
+    }
+
     private float healingTakenModifierPerStack_SpellEngine = 0;
     @Override
     public float getHealingTakenModifierPerStack() {
