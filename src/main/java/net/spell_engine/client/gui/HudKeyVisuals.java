f4d96e8da843113e26e7f70674a8230bc3ecaba8
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-10-30 12:02:13 +0100

    Update widgets

--- src/main/java/net/spell_engine/client/gui/HudKeyVisuals.java
+++ src/main/java/net/spell_engine/client/gui/HudKeyVisuals.java
@@ -6,7 +6,7 @@ import net.spell_engine.SpellEngineMod;
 import java.util.Map;
 
 public class HudKeyVisuals {
-    private static final Drawable.Texture mouseTexture = new Drawable.Texture(new Identifier(SpellEngineMod.ID, "textures/gui/hotkeys.png"), 256, 256);
+    private static final Drawable.Texture mouseTexture = new Drawable.Texture(new Identifier(SpellEngineMod.ID, "textures/gui/widgets.png"), 256, 256);
     public static Map<String, Drawable.Component> custom = Map.ofEntries(
             Map.entry("key.mouse.left", new Drawable.Component(
                     new Drawable.Draw(0, 0, 10, 12),
@@ -30,7 +30,7 @@ public class HudKeyVisuals {
             ))
     );
 
-    private static final int keyboardV = 48;
+    private static final int keyboardV = 32;
     public static final Drawable.Component buttonLeading = new Drawable.Component(
             new Drawable.Draw(0, keyboardV, 3, 12),
             mouseTexture
