a47016f822dcecb1edab66c53ddb421ee515d35f
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-03-16 17:29:50 +0100

    Add golden weapons tag, update loot defaults

--- src/main/java/net/spell_engine/rpg_series/config/Defaults.java
+++ src/main/java/net/spell_engine/rpg_series/config/Defaults.java
@@ -25,13 +25,16 @@ public class Defaults {
 
     static {
         lootConfig = new LootConfig();
+        lootConfig.item_groups.put("golden_weapons", new LootConfig.ItemGroup(
+                List.of("#rpg_series:golden_weapons"),
+                1
+        ).chance(0.3F));
         lootConfig.item_groups.put("weapons_tier_0", new LootConfig.ItemGroup(
                 List.of(weapons(0)),
                 0.25F,
                 0F,
                 1
         ).chance(0.3F));
-
         lootConfig.item_groups.put("weapons_tier_1", new LootConfig.ItemGroup(
                 List.of(weapons(1)),
                 1
@@ -75,16 +78,17 @@ public class Defaults {
                 1
         ).chance(0.5F).enchant());
 
+
+        List.of("minecraft:chests/ruined_portal")
+                .forEach(id -> lootConfig.loot_tables.put(id, List.of("golden_weapons")));
+
         List.of("minecraft:chests/abandoned_mineshaft",
                         "minecraft:chests/igloo_chest",
                         "minecraft:chests/shipwreck_supply",
-                        "minecraft:chests/jungle_temple")
+                        "minecraft:chests/spawn_bonus_chest")
                 .forEach(id -> lootConfig.loot_tables.put(id, List.of("weapons_tier_0")));
 
-        List.of("minecraft:chests/desert_pyramid",
-                        "minecraft:chests/bastion_bridge",
-                        "minecraft:chests/jungle_temple",
-                        "minecraft:chests/pillager_outpost",
+        List.of("minecraft:chests/bastion_bridge",
                         "minecraft:chests/simple_dungeon",
                         "minecraft:chests/stronghold_crossing")
                 .forEach(id -> lootConfig.loot_tables.put(id, List.of("weapons_tier_1")));
@@ -92,20 +96,23 @@ public class Defaults {
         List.of("minecraft:chests/shipwreck_treasure")
                 .forEach(id -> lootConfig.loot_tables.put(id, List.of("armors_tier_1")));
 
-        List.of("minecraft:chests/stronghold_library",
-                        "minecraft:chests/underwater_ruin_big",
+        List.of("minecraft:chests/stronghold_crossing",
+                        "minecraft:chests/desert_pyramid",
+                        "minecraft:chests/underwater_ruin_small",
+                        "minecraft:chests/jungle_temple",
+                        "minecraft:chests/pillager_outpost",
                         "minecraft:chests/woodland_mansion")
                 .forEach(id -> lootConfig.loot_tables.put(id, List.of("weapons_tier_1_enchanted", "armors_tier_1_enchanted")));
 
         List.of("minecraft:chests/bastion_other",
                         "minecraft:chests/nether_bridge",
-                        "minecraft:chests/underwater_ruin_small")
+                        "minecraft:chests/underwater_ruin_big")
                 .forEach(id -> lootConfig.loot_tables.put(id, List.of("weapons_tier_2")));
 
         List.of("minecraft:chests/bastion_treasure",
                         "minecraft:chests/ancient_city",
                         "minecraft:chests/stronghold_library")
-                .forEach(id -> lootConfig.loot_tables.put(id, List.of("armors_tier_2_enchanted")));
+                .forEach(id -> lootConfig.loot_tables.put(id, List.of("armors_tier_2")));
 
         List.of("minecraft:chests/end_city_treasure"
                         )
