7ca7297626564c2a5c3b9bca39059a889e7314d2
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2026-07-11 21:08:25 +0200

    Add some new particle effect

--- common/src/main/java/net/spell_engine/fx/SpellEngineParticles.java
+++ common/src/main/java/net/spell_engine/fx/SpellEngineParticles.java
@@ -216,6 +216,7 @@ public class SpellEngineParticles {
     public static final TemplateEntry area_effect_574 = addAreaEffect(new TemplateEntry("area_effect_574", Texture.of("zone/effect_574", 13)));
     public static final TemplateEntry area_effect_609 = addAreaEffect(new TemplateEntry("area_effect_609", Texture.of("zone/effect_609", 13)));
     public static final TemplateEntry area_effect_658 = addAreaEffect(new TemplateEntry("area_effect_658", Texture.of("zone/effect_658", 16)));
+    public static final TemplateEntry area_effect_700 = addAreaEffect(new TemplateEntry("area_effect_700", Texture.of("zone/effect_700", 22)));
     public static final TemplateEntry area_effect_714 = addAreaEffect(new TemplateEntry("area_effect_714", Texture.of("zone/effect_714", 22)));
     public static final TemplateEntry area_effect_715 = addAreaEffect(new TemplateEntry("area_effect_715", Texture.of("zone/effect_715", 22)));
     public static final TemplateEntry area_effect_741 = addAreaEffect(new TemplateEntry("area_effect_741", Texture.of("zone/effect_741", 23)));
